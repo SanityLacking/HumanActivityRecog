19ac51b1d3c19b9729803f3821f8654e708a3950
cailen robertson <cailen.robertson@griffithuni.edu.au>
2023-01-26 12:06:18 +1000

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+
+train = pd.read_csv("train.csv")
+test = pd.read_csv("test.csv")
